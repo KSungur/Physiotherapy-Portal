ed751b68844d02de950f8dc1d006fc8111dff63c
Kutayberk Sungur <kutayberksungur@gmail.com>
2017-05-15 02:08:19 +0300

    SQL dialect and syntax cleanup

--- src/DoctorMessageScreen.java
+++ src/DoctorMessageScreen.java
@@ -1,75 +1,60 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JButton;
-import javax.swing.border.TitledBorder;
-
-import org.omg.IOP.TAG_MULTIPLE_COMPONENTS;
-
 import net.proteanit.sql.DbUtils;
 
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.awt.event.ActionEvent;
+import java.util.Objects;
 
 public class DoctorMessageScreen extends JFrame {
-	private Connection connection = null;
-	private JPanel contentPane;
-	private JTable tableMessages;
-	private String PatientName = "";
-	private String PatientTCNo = "";
-	
+    private Connection connection = null;
+    private JTable tableMessages;
+    private String PatientName = "";
+    private String PatientTCNo = "";
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					DoctorMessageScreen frame = new DoctorMessageScreen();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the frame.
-	 */
-	public DoctorMessageScreen() {
-		connection = MySqlConn.dbConnector();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 405);
-		setTitle("Messages");
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setViewportBorder(new TitledBorder(null, "Messages", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		scrollPane.setBounds(10, 11, 564, 310);
-		contentPane.add(scrollPane);
-		
-		tableMessages = new JTable();
-		scrollPane.setViewportView(tableMessages);
-		resfreshTable();
-		
-		tableMessages.addMouseListener(new MouseAdapter() {
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                DoctorMessageScreen frame = new DoctorMessageScreen();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    private DoctorMessageScreen() {
+        connection = MySqlConn.dbConnector();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 600, 405);
+        setTitle("Messages");
+        JPanel contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportBorder(new TitledBorder(null, "Messages", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        scrollPane.setBounds(10, 11, 564, 310);
+        contentPane.add(scrollPane);
+
+        tableMessages = new JTable();
+        scrollPane.setViewportView(tableMessages);
+        resfreshTable();
+
+        tableMessages.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent arg0) { //Tikladigimiz rowun verisini aliyoruz
                 try {
@@ -81,10 +66,10 @@ public class DoctorMessageScreen extends JFrame {
                     pst.setString(1, ID);
                     ResultSet rs = pst.executeQuery();
                     while (rs.next()) {
-                    	PatientName = rs.getString("Name");
-                    	PatientTCNo = rs.getString("patientID");
-                        DoctorWriteMessageScreen.DoctorMessageInput(rs.getString("Name"),rs.getString("patientID"));
-                        DoctorReadMessageScreen.DoctorReadMessageInput(rs.getString("Name"),rs.getString("patientID"));
+                        PatientName = rs.getString("Name");
+                        PatientTCNo = rs.getString("patientID");
+                        DoctorWriteMessageScreen.DoctorMessageInput(rs.getString("Name"), rs.getString("patientID"));
+                        DoctorReadMessageScreen.DoctorReadMessageInput(rs.getString("Name"), rs.getString("patientID"));
                     }
 
                     pst.close();
@@ -93,51 +78,41 @@ public class DoctorMessageScreen extends JFrame {
                 }
             }
         });
-		
-		JButton btnShow = new JButton("Show");
-		btnShow.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if(PatientName == "" && PatientTCNo == "")
-				{
-					JOptionPane.showMessageDialog(null, "Hasta Secilmedi");
-				}
-				else 
-				{
-					removeAll();
-	                getContentPane().add(new DoctorReadMessageScreen());
-	                repaint();
-	                invalidate();
-	                revalidate();
-				}
-			}
-		});
-		btnShow.setBounds(184, 332, 89, 23);
-		contentPane.add(btnShow);
-		
-		JButton btnReply = new JButton("Reply");
-		btnReply.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if(PatientName == "" && PatientTCNo == "")
-				{
-					JOptionPane.showMessageDialog(null, "Hasta Secilmedi");
-				}
-				else 
-				{
-					removeAll();
-	                getContentPane().add(new DoctorWriteMessageScreen());
-	                repaint();
-	                invalidate();
-	                revalidate();
-				}
-			}
-		});
-		btnReply.setBounds(294, 332, 89, 23);
-		contentPane.add(btnReply);
-	}
-	
-	private void resfreshTable() {
+
+        JButton btnShow = new JButton("Show");
+        btnShow.addActionListener(arg0 -> {
+            if (Objects.equals(PatientName, "") && Objects.equals(PatientTCNo, "")) {
+                JOptionPane.showMessageDialog(null, "Hasta Secilmedi");
+            } else {
+                removeAll();
+                getContentPane().add(new DoctorReadMessageScreen());
+                repaint();
+                invalidate();
+                revalidate();
+            }
+        });
+        btnShow.setBounds(184, 332, 89, 23);
+        contentPane.add(btnShow);
+
+        JButton btnReply = new JButton("Reply");
+        btnReply.addActionListener(arg0 -> {
+            if (Objects.equals(PatientName, "") && Objects.equals(PatientTCNo, "")) {
+                JOptionPane.showMessageDialog(null, "Hasta Secilmedi");
+            } else {
+                removeAll();
+                getContentPane().add(new DoctorWriteMessageScreen());
+                repaint();
+                invalidate();
+                revalidate();
+            }
+        });
+        btnReply.setBounds(294, 332, 89, 23);
+        contentPane.add(btnReply);
+    }
+
+    private void resfreshTable() {
         try {
-            String query = "SELECT patientID, Name, Surname, Message" +
+            String query = "SELECT patientID, Name, Surname, Message " +
                     "FROM patient " +
                     "ORDER BY Date";
             PreparedStatement pst = connection.prepareStatement(query);
--- src/DoctorWriteMessageScreen.java
+++ src/DoctorWriteMessageScreen.java
@@ -1,135 +1,118 @@
-import javax.swing.JPanel;
-
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.LinkedBlockingDeque;
-
-import javax.swing.JButton;
-import javax.swing.JTextPane;
-import javax.swing.JScrollPane;
-import javax.swing.border.TitledBorder;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import java.awt.event.ActionListener;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.awt.event.ActionEvent;
-import javax.swing.JTextArea;
 
 public class DoctorWriteMessageScreen extends JPanel {
-	private Connection connection;
-	private JTextField tvSubject;
-	private JLabel lblPatientName;
-	private JLabel lblPatientSurname;
-	private JLabel lblPatientID;
-	private JTextArea tvMessage;
-	
-	
-	private static String PatientName;
-	private static String PAtientID;
-	
+    private Connection connection;
+    private JTextField tvSubject;
+    private JLabel lblPatientName;
+    private JLabel lblPatientSurname;
+    private JLabel lblPatientID;
+    private JTextArea tvMessage;
 
-	
-	static void DoctorMessageInput(String patientName, String patientTCNo) {
+
+    private static String PatientName;
+    private static String PatientID;
+
+
+    static void DoctorMessageInput(String patientName, String patientTCNo) {
         PatientName = patientName;
-        PAtientID = patientTCNo;
+        PatientID = patientTCNo;
     }
-	/**
-	 * Create the panel.
-	 */
-	public DoctorWriteMessageScreen() {
-		connection = MySqlConn.dbConnector();
-		setLayout(null);
-		setSize(600,405);
-		
-		JButton btnSend = new JButton("Send");
-		btnSend.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				int reply = JOptionPane.showConfirmDialog(null, "Mesaj Gonderilsin mi", "Confirm", JOptionPane.YES_NO_OPTION);
-	            if (reply == JOptionPane.YES_OPTION) {
-	                try {
-	                    String query = "insert into Message (patientID, Name, Surname, Subject, Message, Date) values (?, ?, ?, ?, ?, ?)";
 
-	                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-	                    LocalDate localDate = LocalDate.now();
+    /**
+     * Create the panel.
+     */
+    DoctorWriteMessageScreen() {
+        connection = MySqlConn.dbConnector();
+        setLayout(null);
+        setSize(600, 405);
 
-	                    PreparedStatement preparedStatement = connection.prepareStatement(query);
-	                    preparedStatement.setString(1, lblPatientID.getText());
-	                    preparedStatement.setString(2, lblPatientName.getText());
-	                    preparedStatement.setString(3, lblPatientSurname.getText());
-	                    preparedStatement.setString(4, tvSubject.getText());
-	                    preparedStatement.setString(5, tvMessage.getText());
-	                    preparedStatement.setString(6, dtf.format(localDate));
+        JButton btnSend = new JButton("Send");
+        btnSend.addActionListener(arg0 -> {
+            int reply = JOptionPane.showConfirmDialog(null, "Sending message?", "Confirm", JOptionPane.YES_NO_OPTION);
+            if (reply == JOptionPane.YES_OPTION) {
+                try {
+                    String query = "INSERT INTO messages (messageTo, Name, Surname, Subject, Content, Date) VALUES (?, ?, ?, ?, ?, ?)";
 
-	                    preparedStatement.execute();
-	                    JOptionPane.showMessageDialog(null, "Mesaj Basari ile iletildi");
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                    LocalDate localDate = LocalDate.now();
 
-	                    preparedStatement.close();
+                    PreparedStatement preparedStatement = connection.prepareStatement(query);
+                    preparedStatement.setString(1, lblPatientID.getText());
+                    preparedStatement.setString(2, lblPatientName.getText());
+                    preparedStatement.setString(3, lblPatientSurname.getText());
+                    preparedStatement.setString(4, tvSubject.getText());
+                    preparedStatement.setString(5, tvMessage.getText());
+                    preparedStatement.setString(6, dtf.format(localDate));
 
-	                } catch (Exception e2) {
-	                    JOptionPane.showMessageDialog(null, e2);
-	                }
-	            } else {
-	                JOptionPane.showMessageDialog(null, "GOODBYE");
-	            }
-			}
-		});
-		btnSend.setBounds(235, 371, 89, 23);
-		add(btnSend);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setViewportBorder(new TitledBorder(null, "Message", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		scrollPane.setBounds(10, 127, 580, 235);
-		add(scrollPane);
-		
-		tvMessage = new JTextArea();
-		scrollPane.setViewportView(tvMessage);
-		
-		JLabel lblSubject = new JLabel("Subject");
-		lblSubject.setBounds(10, 102, 46, 14);
-		add(lblSubject);
-		
-		tvSubject = new JTextField();
-		tvSubject.setBounds(66, 99, 524, 20);
-		add(tvSubject);
-		tvSubject.setColumns(10);
-		
-		lblPatientName = new JLabel("Name");
-		lblPatientName.setBounds(10, 11, 46, 14);
-		add(lblPatientName);
-		
-		lblPatientSurname = new JLabel("Surname");
-		lblPatientSurname.setBounds(10, 36, 46, 14);
-		add(lblPatientSurname);
-		
-		lblPatientID = new JLabel("PatientID");
-		lblPatientID.setBounds(10, 61, 46, 14);
-		add(lblPatientID);
-		fillPatientInfo();
-	}
-	
-	void fillPatientInfo()
-	{
-		 try {
-             String query = "SELECT * FROM patient WHERE patientID = ?";
-             PreparedStatement pst = connection.prepareStatement(query);
-             pst.setString(1, PAtientID);
-             ResultSet rs = pst.executeQuery();
-             
-             lblPatientName.setText(rs.getString("Name"));
-             lblPatientSurname.setText(rs.getString("Surname"));
-             lblPatientID.setText(rs.getString("patientID"));
-             
-             
-             rs.close();
-             pst.close();
-             
-         } catch (Exception e) {
-             e.printStackTrace();
-         }
-	}
+                    preparedStatement.execute();
+                    JOptionPane.showMessageDialog(null, "Message sent");
+
+                    preparedStatement.close();
+
+                } catch (Exception e2) {
+                    JOptionPane.showMessageDialog(null, e2);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "GOODBYE");
+            }
+        });
+        btnSend.setBounds(235, 371, 89, 23);
+        add(btnSend);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportBorder(new TitledBorder(null, "Message", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        scrollPane.setBounds(10, 127, 580, 235);
+        add(scrollPane);
+
+        tvMessage = new JTextArea();
+        scrollPane.setViewportView(tvMessage);
+
+        JLabel lblSubject = new JLabel("Subject");
+        lblSubject.setBounds(10, 102, 46, 14);
+        add(lblSubject);
+
+        tvSubject = new JTextField();
+        tvSubject.setBounds(66, 99, 524, 20);
+        add(tvSubject);
+        tvSubject.setColumns(10);
+
+        lblPatientName = new JLabel("Name");
+        lblPatientName.setBounds(10, 11, 46, 14);
+        add(lblPatientName);
+
+        lblPatientSurname = new JLabel("Surname");
+        lblPatientSurname.setBounds(10, 36, 46, 14);
+        add(lblPatientSurname);
+
+        lblPatientID = new JLabel("PatientID");
+        lblPatientID.setBounds(10, 61, 46, 14);
+        add(lblPatientID);
+        fillPatientInfo();
+    }
+
+    private void fillPatientInfo() {
+        try {
+            String query = "SELECT * FROM patient WHERE patientID = ?";
+            PreparedStatement pst = connection.prepareStatement(query);
+            pst.setString(1, PatientID);
+            ResultSet rs = pst.executeQuery();
+
+            lblPatientName.setText(rs.getString("Name"));
+            lblPatientSurname.setText(rs.getString("Surname"));
+            lblPatientID.setText(rs.getString("patientID"));
+
+
+            rs.close();
+            pst.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
